914cb1d2b2340d52143f402c48ad394d15b809b4
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-10-30 20:04:43 +0900

    build: Update makefile for document for ArisiaCard

--- Script/install_all.mk
+++ Script/install_all.mk
@@ -7,11 +7,18 @@ app_mk   = ../../Script/build_app.mk
 lib_res_dir  =  ArisiaLibrary/Resource/Library/types
 comp_res_dir =  ArisiaComponents/Resource/Library/types
 
-all: lib components tools card
+all: lib doc components tools card
 
 lib: dummy
 	(pushd ArisiaLibrary/Project && make -f $(build_mk) && popd)
 	(pushd ArisiaLibrary/Resource && make && popd)
+
+doc: dummy
+	(pushd ArisiaComponents/Resource && make && popd)
+	(pushd ArisiaCard/Resource && make && popd)
+	(pushd ArisiaCard/Document && make && popd)
+	(pushd Document/Components && make && popd)
+
 
 components: $(comp_res_dir)/ArisiaLibrary.d.ts dummy
 	(pushd ArisiaComponents/Project && make -f $(build_mk) && popd)
